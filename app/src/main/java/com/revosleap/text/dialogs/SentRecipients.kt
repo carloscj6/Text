1b765cbda743c0d88d7df9269c6d95a3fb5fd8e0
carloscj6 <anyona6.cj@gmail.com>
2019-10-18 13:23:20 +0300

    Sent message made more sensible

--- app/src/main/java/com/revosleap/text/dialogs/SentRecipients.kt
+++ app/src/main/java/com/revosleap/text/dialogs/SentRecipients.kt
@@ -1,48 +1,46 @@
 package com.revosleap.text.dialogs
 
-import android.app.Dialog
-import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
-import android.support.v4.app.DialogFragment
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
 import com.revosleap.text.R
 import com.revosleap.text.adapters.RecipientAdapter
 import com.revosleap.text.models.SentMessages
 import kotlinx.android.synthetic.main.sent_recipients_dialog.*
-
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class SentRecipients : BottomSheetDialogFragment() {
     private var sentMessages: SentMessages? = null
 
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.sent_recipients_dialog,container,false)
+        return inflater.inflate(R.layout.sent_recipients_dialog, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setList()
+        textViewMessage?.text = sentMessages?.message
+        imageButtonDismiss?.setOnClickListener {
+            dismiss()
+        }
+        enterText()
+    }
+
+    private fun enterText() {
+        val sdf = SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.getDefault())
+        textViewDateWritten.text = sdf.format(0)
+        textViewDateSent.text = sdf.format(sentMessages?.time)
+        textViewRecipients.text = sentMessages?.contacts?.size.toString()
     }
 
 
-//    override fun onResume() {
-//        super.onResume()
-//        val params = dialog.window!!.attributes
-//        params.width = WindowManager.LayoutParams.WRAP_CONTENT
-//        params.height = WindowManager.LayoutParams.WRAP_CONTENT
-//        dialog.window!!.attributes = params as WindowManager.LayoutParams
-//    }
-
-    private fun setList(){
+    private fun setList() {
         val adapter = RecipientAdapter()
         val manager = LinearLayoutManager(context)
         recyclerViewRecipients.adapter = adapter
@@ -50,7 +48,6 @@ class SentRecipients : BottomSheetDialogFragment() {
         recyclerViewRecipients.hasFixedSize()
         adapter.adItems(sentMessages?.contacts!!)
     }
-
 
 
     companion object {
